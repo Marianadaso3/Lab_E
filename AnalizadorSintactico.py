6fbd718b661d0983e9692cbec1b6a1e98072a6d2
Marianadaso3 <72479030+Marianadaso3@users.noreply.github.com>
2023-05-27 16:50:00 -0600

    terminando analizador sintactico

--- AnalizadorSintactico.py
+++ AnalizadorSintactico.py
@@ -1,4 +1,3 @@
-#falta implementacion de la clase principal
 import graphviz as gv  # Importar la biblioteca graphviz para generar gráficos
 
 class AnalizadorSintactico:
@@ -71,3 +70,76 @@ class AnalizadorSintactico:
                                 hasChanged = True  # Marcar que ha habido cambios en el conjunto
         return newSet  # Devolver el conjunto actualizado
     
+    def analizar(self):
+        tempArray = self.derivations[self.augmentedStartSymbol][0]  # Obtener el cuerpo de la derivación del símbolo inicial aumentado
+        tempArray.insert(0, self.specialSymbol)  # Insertar el símbolo especial al principio del cuerpo de la derivación
+        firstSet = self.closure([[self.augmentedStartSymbol, tempArray]])  # Aplicar la clausura al primer conjunto
+        Iconjunto = [firstSet]  # Lista para almacenar los conjuntos del autómata
+        breakFlag = True  # Variable para controlar el bucle principal
+        while breakFlag:
+            breakFlag = False  # Reiniciar la variable breakFlag
+            for gramatica in Iconjunto.copy():  # Iterar a través de cada gramática en el conjunto de conjuntos
+                for elemento in gramatica:  # Iterar a través de cada elemento en la gramática
+                    _, derivationBody = elemento  # Obtener el cuerpo de la derivación
+                    dotIndex = derivationBody.index(self.specialSymbol)  # Encontrar la posición del símbolo especial
+                    if dotIndex == len(derivationBody) - 1 or dotIndex == -1:  # Si el símbolo especial está al final o no está presente
+                        pass  # Pasar al siguiente elemento
+                    else:
+                        siguiente = derivationBody[dotIndex + 1]  # Obtener el siguiente símbolo después del símbolo especial
+                        tgoto = self.goto(gramatica, siguiente)  # Calcular el conjunto siguiente mediante la función 'goto'
+                        if tgoto not in Iconjunto and tgoto:  # Si el conjunto siguiente no está en el conjunto de conjuntos y no está vacío
+                            Iconjunto.append(tgoto)  # Agregar el conjunto siguiente al conjunto de conjuntos
+                            self.index = Iconjunto.index(gramatica)  # Obtener el índice del conjunto actual
+                            if self.automatonTransitions.get((self.index, siguiente)) == None:  # Si la transición no existe en el autómata
+                                self.automatonTransitions[(self.index, siguiente)] = [Iconjunto.index(tgoto)]  # Agregar la transición al autómata
+                            else:  # Si la transición ya existe en el autómata
+                                self.automatonTransitions[(self.index, siguiente)].append(Iconjunto.index(tgoto))  # Agregar el índice del conjunto siguiente a la transición existente
+                            breakFlag = True  # Marcar que ha habido cambios en el bucle principal
+                        else:
+                            if tgoto:  # Si el conjunto siguiente no está vacío
+                                self.index = Iconjunto.index(gramatica)  # Obtener el índice del conjunto actual
+                                if self.automatonTransitions.get((self.index, siguiente)) == None:  # Si la transición no existe en el autómata
+                                    self.automatonTransitions[(self.index, siguiente)] = [Iconjunto.index(tgoto)]  # Agregar la transición al autómata
+                                else:  # Si la transición ya existe en el autómata
+                                    if Iconjunto.index(tgoto) not in self.automatonTransitions[(self.index, siguiente)]:  # Si el índice del conjunto siguiente no está en la transición existente
+                                        self.automatonTransitions[(self.index, siguiente)].append(Iconjunto.index(tgoto))  # Agregar el índice del conjunto siguiente a la transición existente
+                                    
+        self.ISET = Iconjunto  # Establecer el conjunto de conjuntos del autómata
+        
+    def formatAutomatonLR0(self):
+        res = {}  # Diccionario para almacenar los nombres de los conjuntos y sus etiquetas
+        for i in range(len(self.ISET)):  # Iterar a través de los índices del conjunto de conjuntos
+            Iname = f"I{i}"  # Nombre del conjunto
+            Ilabel = ""  # Etiqueta del conjunto
+            for item in self.ISET[i]:  # Iterar a través de cada elemento en el conjunto
+                derivationKey, derivationBody = item  # Obtener la clave de la derivación y el cuerpo de la derivación
+                if derivationKey == self.augmentedStartSymbol and derivationBody.index(self.specialSymbol) == len(derivationBody) - 1:  # Si la clave de la derivación es el símbolo inicial aumentado y el símbolo especial está al final
+                    Ilabel += f"{derivationKey} : {self.specialSymbol};\n"  # Agregar la línea a la etiqueta del conjunto
+                else:
+                    if derivationBody.index(self.specialSymbol) == len(derivationBody) - 1:  # Si el símbolo especial está al final
+                        Ilabel += f"{derivationKey} : {self.specialSymbol} • ;\n"  # Agregar la línea a la etiqueta del conjunto
+                    else:
+                        dotIndex = derivationBody.index(self.specialSymbol)  # Encontrar la posición del símbolo especial
+                        Ilabel += f"{derivationKey} : {' '.join(derivationBody[:dotIndex])} {self.specialSymbol} {' '.join(derivationBody[dotIndex+1:])} ;\n"  # Agregar la línea a la etiqueta del conjunto
+            res[Iname] = Ilabel  # Agregar el nombre del conjunto y su etiqueta al diccionario
+        return res  # Devolver el diccionario de nombres de conjuntos y etiquetas
+    
+    def printISet(self):
+        # Imprime los conjuntos I del autómata LR(0)
+        for i in range(len(self.ISET)):
+            print(f"I{i} = {self.ISET[i]}")
+
+    def graphAutomatonLR0(self):
+        # Genera el gráfico del autómata LR(0)
+        g = gv.Digraph(format='png')
+        values = self.formatAutomatonLR0()
+        for key in values:
+            # Crea un nodo para cada conjunto I del autómata LR(0)
+            g.node(key, label=values[key])
+        for key in self.automatonTransitions:
+            item, element = key
+            for value in self.automatonTransitions[key]:
+                # Crea una transición entre dos conjuntos I del autómata LR(0)
+                g.edge(f"I{item}", f"I{value}", label=element)
+        # Renderiza y muestra el gráfico del autómata LR(0)
+        g.render('output/automatonLR0.gv', view=True)
